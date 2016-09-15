b0687035a2a8bc8b21a3fb23b89f4198efa3aadc
Tianyu Gao <tianyug1@andrew.cmu.edu>
2016-09-15 15:28:06 -0400

    db7,8

---
+++ database/db7/buk-vac-img4/submit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from vasp import Vasp
+from ase.md.langevin import Langevin
+from ase.io.trajectory import Trajectory
+from ase import units
+from amp import Amp
+
+calc = Vasp("./../../../NDFT/bulk=fcc/type=neb/supercell=222/image=4")
+
+atoms = calc.get_atoms()
+
+atoms.set_calculator(Amp(load="./../../../networks/db7/24-24/checkpoint-parameters.json"))
+
+dyn = Langevin(atoms, 5 * units.fs, 900 * units.kB, 0.002)
+
+
+def printenergy(a=atoms):
+    epot = a.get_potential_energy() / len(a)
+    ekin = a.get_kinetic_energy() / len(a)
+    
+dyn.attach(printenergy, interval=5)
+
+traj = Trajectory("./MD.traj", "w", atoms)
+dyn.attach(traj.write, interval=5)
+
+dyn.run(100)
