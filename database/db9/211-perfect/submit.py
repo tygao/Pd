1c65b53d5e2688fdfeb0e1df3136d6f47ebefdb9
Tianyu Gao <tianyug1@andrew.cmu.edu>
2016-09-23 11:02:38 -0400

    db9

---
+++ database/db9/211-perfect/submit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from vasp import Vasp
+from ase.md.langevin import Langevin
+from ase.io.trajectory import Trajectory
+from ase import units
+from amp import Amp
+
+calc = Vasp("./../../../NDFT/surface=fcc211//type=relaxation/supercell=33")
+
+atoms = calc.get_atoms()
+
+atoms.set_calculator(Amp(load="./../../../networks/db9/15-15/checkpoint-parameters.json"))
+
+dyn = Langevin(atoms, 5 * units.fs, 900 * units.kB, 0.002)
+
+
+def printenergy(a=atoms):
+    epot = a.get_potential_energy() / len(a)
+    ekin = a.get_kinetic_energy() / len(a)
+    
+dyn.attach(printenergy, interval=5)
+
+traj = Trajectory("./MD.traj", "w", atoms)
+dyn.attach(traj.write, interval=5)
+
+dyn.run(500)
